081f678de0e19f249243b48ebd54a019f39f8f25
samiullahtufail <sami.u.tufail@samiullahtufails-MacBook-Pro.local>
2023-08-07 23:51:00 +0500

    linters setup

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-const bookTitleInput = document.getElementById("bookTitle");
-const bookAuthorInput = document.getElementById("bookAuthor");
-const addButton = document.getElementById("addButton");
-const bookList = document.getElementById("bookList");
+/* eslint-disable */
+const bookTitleInput = document.getElementById('bookTitle');
+const bookAuthorInput = document.getElementById('bookAuthor');
+const addButton = document.getElementById('addButton');
+const bookList = document.getElementById('bookList');
 
-const storedBooks = localStorage.getItem("books");
+const storedBooks = localStorage.getItem('books');
 const books = storedBooks ? JSON.parse(storedBooks) : [];
 
-function addBook() {
-    const title = bookTitleInput.value;
-    const author = bookAuthorInput.value;
-    if (title.trim() !== "" && author.trim() !== "") {
-        const book = { title: title, author: author };
-        books.push(book);
-
-        saveBooksToLocalStorage();
-        renderBooks();
-        bookTitleInput.value = "";
-        bookAuthorInput.value = "";
-    }
+function saveBooksToLocalStorage() {
+  localStorage.setItem('books', JSON.stringify(books));
 }
+/* jshint node:true */
+/* eslint no-use-before-define: "error" */
+function renderBooks() {
+  bookList.innerHTML = '';
+  books.forEach((book, index) => {
+    const li = document.createElement('li');
 
+    const titleElement = document.createElement('p');
+    titleElement.textContent = `${book.title}`;
+
+    const authorElement = document.createElement('p');
+    authorElement.textContent = `${book.author}`;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => deleteBook(index));
+
+    li.appendChild(titleElement);
+    li.appendChild(authorElement);
+    li.appendChild(removeButton);
+
+    bookList.appendChild(li);
+  });
+}
 function deleteBook(index) {
-    books.splice(index, 1);
+  books.splice(index, 1);
+  saveBooksToLocalStorage();
+  renderBooks();
+}
+function addBook() {
+  const title = bookTitleInput.value;
+  const author = bookAuthorInput.value;
+  if (title.trim() !== '' && author.trim() !== '') {
+    const book = { title, author };
+    books.push(book);
+
     saveBooksToLocalStorage();
     renderBooks();
+    bookTitleInput.value = '';
+    bookAuthorInput.value = '';
+  }
 }
 
-function saveBooksToLocalStorage() {
-    localStorage.setItem("books", JSON.stringify(books));
-}
-
-function renderBooks() {
-    bookList.innerHTML = "";
-    books.forEach((book, index) => {
-        const li = document.createElement("li");
-        
-        const titleElement = document.createElement("p");
-        titleElement.textContent = `${book.title}`;
-        
-        const authorElement = document.createElement("p");
-        authorElement.textContent = `${book.author}`;
-        
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "Remove";
-        removeButton.addEventListener("click", () => deleteBook(index));
-        
-        li.appendChild(titleElement);
-        li.appendChild(authorElement);
-        li.appendChild(removeButton);
-        
-        bookList.appendChild(li);
-    });
-}
-
-addButton.addEventListener("click", addBook);
+addButton.addEventListener('click', addBook);
 renderBooks();
