b13cc0c8592df3e8d448bfb358265a0a7e263531
samiullahtufail <sami.u.tufail@samiullahtufails-MacBook-Pro.local>
2023-08-09 02:48:40 +0500

    refractor and classes

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
 /* eslint-disable */
-const bookTitleInput = document.getElementById('bookTitle');
-const bookAuthorInput = document.getElementById('bookAuthor');
-const addButton = document.getElementById('addButton');
-const bookList = document.getElementById('bookList');
 
-const storedBooks = localStorage.getItem('books');
-const books = storedBooks ? JSON.parse(storedBooks) : [];
-
-function saveBooksToLocalStorage() {
-  localStorage.setItem('books', JSON.stringify(books));
-}
-/* jshint node:true */
-/* eslint no-use-before-define: "error" */
-function renderBooks() {
-  bookList.innerHTML = '';
-  books.forEach((book, index) => {
-    const li = document.createElement('li');
-
-    const titleElement = document.createElement('p');
-    titleElement.textContent = `${book.title}`;
-
-    const authorElement = document.createElement('p');
-    authorElement.textContent = `${book.author}`;
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => deleteBook(index));
-
-    li.appendChild(titleElement);
-    li.appendChild(authorElement);
-    li.appendChild(removeButton);
-
-    bookList.appendChild(li);
-  });
-}
-function deleteBook(index) {
-  books.splice(index, 1);
-  saveBooksToLocalStorage();
-  renderBooks();
-}
-function addBook() {
-  const title = bookTitleInput.value;
-  const author = bookAuthorInput.value;
-  if (title.trim() !== '' && author.trim() !== '') {
-    const book = { title, author };
-    books.push(book);
-
-    saveBooksToLocalStorage();
-    renderBooks();
-    bookTitleInput.value = '';
-    bookAuthorInput.value = '';
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 }
 
-addButton.addEventListener('click', addBook);
-renderBooks();
+class BookList {
+  constructor() {
+    this.books = this.loadBooksFromLocalStorage();
+    this.bookTitleInput = document.getElementById('bookTitle');
+    this.bookAuthorInput = document.getElementById('bookAuthor');
+    this.addButton = document.getElementById('addButton');
+    this.bookList = document.getElementById('bookList');
+
+    this.renderBooks();
+    this.setupEventListeners();
+  }
+
+  loadBooksFromLocalStorage() {
+    const storedBooks = localStorage.getItem('books');
+    return storedBooks ? JSON.parse(storedBooks) : [];
+  }
+
+  saveBooksToLocalStorage() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  renderBooks() {
+    this.bookList.innerHTML = '';
+    this.books.forEach((book, index) => {
+      const li = document.createElement('li');
+      li.classList.add('book-item'); // Add a CSS class for styling
+  
+      // Create a <span> element for the book information
+      const bookInfo = document.createElement('span');
+      bookInfo.textContent = `${book.title} by ${book.author} `;
+  
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => this.deleteBook(index));
+  
+      // Append both elements to the list item
+      li.appendChild(bookInfo);
+      li.appendChild(removeButton);
+  
+      this.bookList.appendChild(li);
+    });
+  }
+  
+
+  deleteBook(index) {
+    this.books.splice(index, 1);
+    this.saveBooksToLocalStorage();
+    this.renderBooks();
+  }
+
+  addBook() {
+    const title = this.bookTitleInput.value;
+    const author = this.bookAuthorInput.value;
+    if (title.trim() !== '' && author.trim() !== '') {
+      const book = new Book(title, author);
+      this.books.push(book);
+
+      this.saveBooksToLocalStorage();
+      this.renderBooks();
+
+      this.bookTitleInput.value = '';
+      this.bookAuthorInput.value = '';
+    }
+  }
+
+  setupEventListeners() {
+    this.addButton.addEventListener('click', () => this.addBook());
+  }
+}
+
+// Create an instance of BookList to initialize the app
+const bookList = new BookList();
