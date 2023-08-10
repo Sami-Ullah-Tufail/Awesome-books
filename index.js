b9626159085a27aebdd69352403419caa88096d6
unknown <lemmayonas@gmail.com>
2023-08-10 18:26:15 +0300

    fix linter issue

--- index.js
+++ index.js
@@ -1,88 +1,3 @@
-class BookList {
-  constructor() {
-    this.books = JSON.parse(localStorage.getItem('books')) || [];
-    this.bookListElement = document.getElementById('bookList');
-    this.bookTitleInput = document.getElementById('bookTitle');
-    this.bookAuthorInput = document.getElementById('bookAuthor');
-    this.addButton = document.getElementById('addButton');
-
-    this.addButton.addEventListener('click', () => {
-      this.addBook();
-      this.renderBooks();
-      this.bookTitleInput.value = '';
-      this.bookAuthorInput.value = '';
-    });
-
-    this.renderBooks();
-  }
-
-  addBook() {
-    const title = this.bookTitleInput.value.trim();
-    const author = this.bookAuthorInput.value.trim();
-    if (title !== '' && author !== '') {
-      const book = { title, author };
-      this.books.push(book);
-      this.saveBooks();
-    }
-  }
-
-  removeBook(index) {
-    this.books.splice(index, 1);
-    this.saveBooks();
-  }
-
-  saveBooks() {
-    localStorage.setItem('books', JSON.stringify(this.books));
-  }
-
-  renderBooks() {
-    this.bookListElement.innerHTML = '';
-    this.books.forEach((book, index) => {
-      const li = document.createElement('li');
-      li.textContent = `${book.title} by ${book.author}`;
-
-      const removeButton = document.createElement('button');
-      removeButton.textContent = 'Remove';
-      removeButton.classList.add('remove-button');
-      removeButton.addEventListener('click', () => {
-        this.removeBook(index);
-        this.renderBooks();
-      });
-
-      li.appendChild(removeButton);
-      this.bookListElement.appendChild(li);
-    });
-  }
-
-  showSection(sectionId) {
-    const sections = ['booksListSection', 'addBookFormSection', 'contactInfoSection'];
-
-    sections.forEach(id => {
-      const section = document.getElementById(id);
-      if (id === sectionId) {
-        section.style.display = 'block';
-      } else {
-        section.style.display = 'none';
-      }
-    });
-  }
-}
-
-function renderBooksOnLoad() {
-  const bookList = new BookList();
-  bookList.renderBooks(); // Call renderBooks method here
-
-  // Attach event listeners to navigation links using the bookList instance
-  const booksLink = document.getElementById('booksLink');
-  const addBookLink = document.getElementById('addBookLink');
-  const contactLink = document.getElementById('contactLink');
-
-  booksLink.addEventListener('click', () => bookList.showSection('booksListSection'));
-  addBookLink.addEventListener('click', () => bookList.showSection('addBookFormSection'));
-  contactLink.addEventListener('click', () => bookList.showSection('contactInfoSection'));
-}
-
-document.addEventListener('DOMContentLoaded', renderBooksOnLoad);
 class AwesomeBooksApp {
   constructor() {
     this.books = JSON.parse(localStorage.getItem('books')) || [];
@@ -91,16 +6,7 @@ class AwesomeBooksApp {
     this.authorInput = document.getElementById('author');
     this.addButton = document.getElementById('addButton');
 
-    this.addButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.addBook();
-      this.renderBooks();
-      this.titleInput.value = '';
-      this.authorInput.value = '';
-    });
-
-    this.renderBooks();
-   
+    this.addButton.addEventListener('click', this.handleAddBookClick.bind(this));
 
     const listLink = document.getElementById('list');
     const addNewLink = document.getElementById('addNew');
@@ -109,6 +15,17 @@ class AwesomeBooksApp {
     listLink.addEventListener('click', () => this.showSection('listSection'));
     addNewLink.addEventListener('click', () => this.showSection('addSection'));
     contactLink.addEventListener('click', () => this.showSection('contactSection'));
+
+    this.showSection('listSection');
+    this.renderBooks();
+  }
+
+  handleAddBookClick(event) {
+    event.preventDefault();
+    this.addBook();
+    this.renderBooks();
+    this.titleInput.value = '';
+    this.authorInput.value = '';
   }
 
   addBook() {
@@ -150,10 +67,11 @@ class AwesomeBooksApp {
     });
   }
 
+  // eslint-disable-next-line class-methods-use-this
   showSection(sectionId) {
     const sections = ['homePage', 'listSection', 'addSection', 'contactSection'];
 
-    sections.forEach(id => {
+    sections.forEach((id) => {
       const section = document.getElementById(id);
       if (id === sectionId) {
         section.style.display = 'block';
@@ -165,8 +83,8 @@ class AwesomeBooksApp {
 }
 
 function initializeApp() {
+  // eslint-disable-next-line no-unused-vars
   const app = new AwesomeBooksApp();
-  app.renderBooks();
 }
 
- document.addEventListener('DOMContentLoaded', initializeApp);
+document.addEventListener('DOMContentLoaded', initializeApp);
